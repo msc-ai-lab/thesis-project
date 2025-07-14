42935ea114123b203a6cf1fc4dc1e61c5be2d61e
Avishek Dangol <dangolavishek@gmail.com>
2025-07-14 15:41:49 +0100

    chore: removed train_split.csv

--- scd/setup.py
+++ scd/setup.py
@@ -5,4 +5,14 @@ setup(
   version='0.1.0',
   description='An LLM-enhanced skin cancer detection application using convolutional neural networks and explainable AI.',
   packages=find_packages(include=['scd', 'scd.*']),
+  install_requires=[
+    'albumentations',
+    'torch',
+    'captum',
+    'matplotlib',
+    'numpy',
+    'Pillow',
+    # TODO: add model dependencies
+  ],
+  python_requires='>=3.8',
 )
